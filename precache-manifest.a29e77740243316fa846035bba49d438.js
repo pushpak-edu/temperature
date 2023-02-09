cf8c06c8a78070aff974318fdb900dffa0a35b9e
Pushpak Sharma <pushpaksharma@Pushpaks-Air.ht.home>
2023-02-09 11:35:00 +0530

    Updates

---
+++ precache-manifest.a29e77740243316fa846035bba49d438.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15d5343179a975a2a941d3d832530d64",
+    "url": "/temperature/index.html"
+  },
+  {
+    "revision": "4d1007ef83db0522d8a3",
+    "url": "/temperature/static/css/main.81e6fef5.chunk.css"
+  },
+  {
+    "revision": "b57c39673f650a7a3894",
+    "url": "/temperature/static/js/2.6f4f9af9.chunk.js"
+  },
+  {
+    "revision": "4d1007ef83db0522d8a3",
+    "url": "/temperature/static/js/main.0a1f7b15.chunk.js"
+  },
+  {
+    "revision": "94b2bbbc988780c444f1",
+    "url": "/temperature/static/js/runtime~main.4141a3e5.js"
+  }
+]);
